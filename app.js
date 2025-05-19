831ecc08fb4b429b64188bcab19813d0b2c02c38
Abhishek Kumar Gupta <abhishekprince9109@gmail.com>
2025-05-19 16:43:41 +0530

    final sample

--- app.js
+++ app.js
@@ -33,26 +33,6 @@ app.use(express.json());
 app.engine("ejs",ejsMate);
 app.use(express.static(path.join(__dirname,"public")));
 
-const Listing= require("./models/listing.js");
-const initData=require("./init/data.js");
-
-let initDB= async ()=>{
-    await Listing.deleteMany({});
-    initData.data= initData.data.map((obj)=>(
-      {...obj, owner:"67e8039796677779396aa0b9"}));
-    await Listing.insertMany(initData.data);
-    console.log("data was saved");
-    
-}
-
-app.get("/addData",async(req,res)=>{
-    initDB();
-    res.send("data saved !!!!!");
-});
-
-
-
-
 
 // let mongo_url="mongodb://127.0.0.1:27017/wanderlust";
 
